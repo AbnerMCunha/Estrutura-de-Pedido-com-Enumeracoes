d1dee76ecb7624bc27e6b9ba207f88184d644407
Abner Cunha <38359487+AbnerMCunha@users.noreply.github.com>
2022-02-18 14:11:30 -0300

    Add files via upload

---
+++ Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birth) : this()
+        {
+            Name = name;
+            Email = email;
+            Birth = birth;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + "\nEmail: " + Email + "Birth Date: " + Birth.ToString() ;
+        }
+
+    }
+}
