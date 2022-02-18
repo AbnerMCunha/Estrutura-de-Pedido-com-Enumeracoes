e620ae2325b58150099a0571eafa12ea5e0292f8
Abner Cunha <38359487+AbnerMCunha@users.noreply.github.com>
2022-02-18 14:06:54 -0300

    Add files via upload

---
+++ OrderItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public Product Produto { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product produto)
+        {
+            Quantity = quantity;
+            Price = price;
+            Produto = produto;
+        }
+
+        public double Total()
+        {
+            return Quantity* Price;
+        }
+
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price) : this()
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", $ " + Price.ToString("F2" , CultureInfo.InvariantCulture )  ;
+        }
+
+    }
+}
